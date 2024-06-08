94b67ad8bc301f0f4f8d4c821a33f767bf86d12c
wakame-tech <kamata1919@gmail.com>
2024-06-08 21:04:59 +0900

    clean: repo_path

--- src/git_repository.rs
+++ src/git_repository.rs
@@ -4,33 +4,31 @@ use std::fs::{self, OpenOptions};
 use std::io::Write;
 use std::path::PathBuf;
 
-pub fn repo_path(repo: &GitRepository, paths: Vec<PathBuf>) -> PathBuf {
-    paths
-        .into_iter()
-        .fold(repo.gitdir.clone(), |acc, p| acc.join(p))
+pub fn repo_path(gitdir: &PathBuf, paths: &[PathBuf]) -> PathBuf {
+    paths.iter().fold(gitdir.clone(), |acc, p| acc.join(p))
 }
 
 pub fn repo_file(
-    repo: &GitRepository,
-    paths: Vec<PathBuf>,
+    gitdir: &PathBuf,
+    paths: &[PathBuf],
     mkdir: Option<bool>,
 ) -> Result<Option<PathBuf>> {
     // Python: *path[:-1]
     // Rust: paths[..n].to_vec()
-    if let Some(_) = repo_dir(repo, paths[..paths.len() - 1].to_vec(), mkdir)? {
-        Ok(Some(repo_path(repo, paths)))
+    if let Some(_) = repo_dir(gitdir, &paths[..paths.len() - 1], mkdir)? {
+        Ok(Some(repo_path(gitdir, paths)))
     } else {
         Ok(None)
     }
 }
 
 pub fn repo_dir(
-    repo: &GitRepository,
-    paths: Vec<PathBuf>,
+    gitdir: &PathBuf,
+    paths: &[PathBuf],
     mkdir: Option<bool>,
 ) -> Result<Option<PathBuf>> {
     let mkdir = mkdir.unwrap_or(false);
-    let path: PathBuf = repo_path(repo, paths);
+    let path: PathBuf = repo_path(gitdir, paths);
 
     if path.exists() {
         if path.is_dir() {
@@ -63,31 +61,31 @@ pub fn repo_create(path: &PathBuf) -> Result<GitRepository> {
     }
     fs::create_dir_all(&repo.worktree)?;
 
-    repo_dir(&repo, vec![PathBuf::from("branches")], Some(true))?;
-    repo_dir(&repo, vec![PathBuf::from("objects")], Some(true))?;
+    repo_dir(&repo.gitdir, &[PathBuf::from("branches")], Some(true))?;
+    repo_dir(&repo.gitdir, &[PathBuf::from("objects")], Some(true))?;
     repo_dir(
-        &repo,
-        vec![PathBuf::from("refs"), PathBuf::from("tags")],
+        &repo.gitdir,
+        &[PathBuf::from("refs"), PathBuf::from("tags")],
         Some(true),
     )?;
     repo_dir(
-        &repo,
-        vec![PathBuf::from("refs"), PathBuf::from("heads")],
+        &repo.gitdir,
+        &[PathBuf::from("refs"), PathBuf::from("heads")],
         Some(true),
     )?;
 
     // ["description"][:-1] は [] になって ./test/.git になるがそうなって欲しくない
-    if let Some(path) = repo_file(&repo, vec![PathBuf::from("description")], Some(false))? {
+    if let Some(path) = repo_file(&repo.gitdir, &[PathBuf::from("description")], Some(false))? {
         let mut f = OpenOptions::new().write(true).create(true).open(path)?;
         f.write(b"Unnamed repository; edit this file 'description' to name the repository.\n")?;
     }
 
-    if let Some(path) = repo_file(&repo, vec![PathBuf::from("HEAD")], Some(false))? {
+    if let Some(path) = repo_file(&repo.gitdir, &[PathBuf::from("HEAD")], Some(false))? {
         let mut f = OpenOptions::new().write(true).create(true).open(path)?;
         f.write(b"ref: refs/heads/master\n")?;
     }
 
-    if let Some(path) = repo_file(&repo, vec![PathBuf::from("config")], Some(false))? {
+    if let Some(path) = repo_file(&repo.gitdir, &[PathBuf::from("config")], Some(false))? {
         let conf = GitConfig::default();
         conf.write(&path)?;
     }
@@ -113,7 +111,7 @@ impl GitRepository {
             gitdir,
         };
 
-        if let Some(path) = repo_file(&repo, vec![PathBuf::from("config")], Some(false))? {
+        if let Some(path) = repo_file(&repo.gitdir, &[PathBuf::from("config")], Some(false))? {
             if !force {
                 let conf = GitConfig::read(&path)?;
                 if conf.repository_format_version != 0 {
