099afc4999ab63148ac396977cb93b1b39c98773
wakame-tech <kamata1919@gmail.com>
2024-06-22 14:12:14 +0900

    refactor: git init

--- src/git_repository.rs
+++ src/git_repository.rs
@@ -4,91 +4,52 @@ use std::fs::{self, OpenOptions};
 use std::io::Write;
 use std::path::PathBuf;
 
-pub fn repo_path(gitdir: &PathBuf, paths: &[PathBuf]) -> PathBuf {
-    paths.iter().fold(gitdir.clone(), |acc, p| acc.join(p))
-}
-
-pub fn repo_file(
-    gitdir: &PathBuf,
-    paths: &[PathBuf],
-    mkdir: Option<bool>,
-) -> Result<Option<PathBuf>> {
-    // Python: *path[:-1]
-    // Rust: paths[..n].to_vec()
-    if let Some(_) = repo_dir(gitdir, &paths[..paths.len() - 1], mkdir)? {
-        Ok(Some(repo_path(gitdir, paths)))
-    } else {
-        Ok(None)
-    }
-}
-
-pub fn repo_dir(
-    gitdir: &PathBuf,
-    paths: &[PathBuf],
-    mkdir: Option<bool>,
-) -> Result<Option<PathBuf>> {
-    let mkdir = mkdir.unwrap_or(false);
-    let path: PathBuf = repo_path(gitdir, paths);
-
-    if path.exists() {
-        if path.is_dir() {
-            return Ok(Some(path));
-        } else {
-            return Err(anyhow::anyhow!("Not a directory {:?}", path));
-        }
-    }
-
-    if mkdir {
-        fs::create_dir_all(&path)?;
-        Ok(Some(path))
-    } else {
-        Ok(None)
-    }
+fn touch_file(file_path: &PathBuf, content: &[u8]) -> Result<()> {
+    let mut f = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .open(file_path)?;
+    f.write(content)?;
+    Ok(())
 }
 
 pub fn repo_create(path: &PathBuf) -> Result<GitRepository> {
     let repo = GitRepository::new(path.clone(), Some(true))?;
     if repo.worktree.exists() {
-        if !repo.worktree.is_dir() {
-            return Err(anyhow::anyhow!("Not a directory {:?}", repo.worktree));
-        }
-        if repo.gitdir.exists() && repo.gitdir.read_dir()?.count() > 0 {
-            return Err(anyhow::anyhow!(
-                "Git dir does not exists of not empty {:?}",
-                repo.gitdir
-            ));
-        }
+        anyhow::ensure!(
+            repo.worktree.is_dir(),
+            "Not a directory {:?}",
+            repo.worktree
+        );
+        anyhow::ensure!(
+            !(repo.gitdir.exists() && repo.gitdir.read_dir()?.count() > 0),
+            "Git dir does not exists or not empty {:?}",
+            repo.gitdir
+        );
     }
     fs::create_dir_all(&repo.worktree)?;
 
-    repo_dir(&repo.gitdir, &[PathBuf::from("branches")], Some(true))?;
-    repo_dir(&repo.gitdir, &[PathBuf::from("objects")], Some(true))?;
-    repo_dir(
-        &repo.gitdir,
-        &[PathBuf::from("refs"), PathBuf::from("tags")],
-        Some(true),
-    )?;
-    repo_dir(
-        &repo.gitdir,
-        &[PathBuf::from("refs"), PathBuf::from("heads")],
-        Some(true),
-    )?;
+    fs::create_dir(&repo.gitdir)?;
+    for dir in [
+        repo.gitdir.join("branches"),
+        repo.gitdir.join("objects"),
+        repo.gitdir.join("refs").join("tags"),
+        repo.gitdir.join("refs").join("heads"),
+    ] {
+        if dir.exists() {
+            anyhow::ensure!(dir.is_dir(), "Not a directory {:?}", path);
+        }
+        fs::create_dir_all(&dir)?;
+    }
 
     // ["description"][:-1] は [] になって ./test/.git になるがそうなって欲しくない
-    if let Some(path) = repo_file(&repo.gitdir, &[PathBuf::from("description")], Some(false))? {
-        let mut f = OpenOptions::new().write(true).create(true).open(path)?;
-        f.write(b"Unnamed repository; edit this file 'description' to name the repository.\n")?;
-    }
-
-    if let Some(path) = repo_file(&repo.gitdir, &[PathBuf::from("HEAD")], Some(false))? {
-        let mut f = OpenOptions::new().write(true).create(true).open(path)?;
-        f.write(b"ref: refs/heads/master\n")?;
-    }
-
-    if let Some(path) = repo_file(&repo.gitdir, &[PathBuf::from("config")], Some(false))? {
-        let conf = GitConfig::default();
-        conf.write(&path)?;
-    }
+    touch_file(
+        &repo.gitdir.join("description"),
+        b"Unnamed repository; edit this file 'description' to name the repository.\n",
+    )?;
+    touch_file(&repo.gitdir.join("HEAD"), b"ref: refs/heads/master\n")?;
+    let conf = GitConfig::default();
+    conf.write(&repo.gitdir.join("config"))?;
     Ok(repo)
 }
 
@@ -102,25 +63,21 @@ impl GitRepository {
         let force = force.unwrap_or(false);
         let gitdir = path.join(".git");
 
-        if !(force || gitdir.is_dir()) {
-            return Err(anyhow::anyhow!("not a git repository: {:?}", path));
-        }
+        anyhow::ensure!(force || gitdir.is_dir(), "not a git repository: {:?}", path);
 
         let repo = Self {
             worktree: path,
             gitdir,
         };
 
-        if let Some(path) = repo_file(&repo.gitdir, &[PathBuf::from("config")], Some(false))? {
-            if !force {
-                let conf = GitConfig::read(&path)?;
-                if conf.repository_format_version != 0 {
-                    return Err(anyhow::anyhow!(
-                        "Unsupported repositoryformatversion {:?}",
-                        conf.repository_format_version
-                    ));
-                }
-            }
+        let path = &repo.gitdir.join("config");
+        if !force {
+            let conf = GitConfig::read(&path)?;
+            anyhow::ensure!(
+                conf.repository_format_version == 0,
+                "Unsupported repositoryformatversion {:?}",
+                conf.repository_format_version
+            )
         }
 
         Ok(repo)
